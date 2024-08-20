6a6dd762083d51f332bf74314b0c1b6b6966576b
George Tislenko <42586943+drumslave-git@users.noreply.github.com>
2024-08-20 18:22:40 +0300

    more logs

--- start.sh
+++ start.sh
@@ -2,7 +2,9 @@
 if [ ! -f /app/config/data.db ]; then
   echo "Database not found, creating..."
   touch /app/config/data.db
-  npx prisma db push
+  ls -la /app/config
+  echo "data.db created"
+  npx --yes prisma db push
 fi
 echo "Starting server..."
 node server.js

80c698419eadd7dff10d51353fff35afad5ecbae
George Tislenko <42586943+drumslave-git@users.noreply.github.com>
2024-08-26 10:19:05 +0300

    logs

--- src/app/api/app/route.ts
+++ src/app/api/app/route.ts
@@ -3,8 +3,10 @@ import {App} from "@prisma/client"
 
 export async function GET(req: Request) {
   const apps = await prisma.app.findMany()
-  return Response.json(apps.map((app: App) => {
+  const noKeyApps = apps.map((app: App) => {
+    console.log(app.name)
     const {api_key, ...rest} = app
     return rest
-  }))
+  })
+  return Response.json(noKeyApps)
 }

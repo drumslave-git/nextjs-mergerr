9aaee06ec138462a201b224a0e1b20a470d15971
George Tislenko <42586943+drumslave-git@users.noreply.github.com>
2024-08-26 17:11:40 +0300

    winston

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,13 @@
 import { PrismaClient } from '@prisma/client'
+import { logger } from '@/lib/logger'
 
 const prisma = new PrismaClient()
+
+prisma.app.findMany().then(apps => {
+  logger.info(`Found ${apps.length} apps:`)
+  for (const app of apps) {
+    logger.info(app.name)
+  }
+})
 
 export {prisma}
